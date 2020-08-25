a07a98209dadc98ece7687ca6b554cb9e8baaf17
katyvonkaty <kj0203@gmail.com>
2020-08-25 11:25:12 -0400

    updated

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
 const btnShare = document.getElementById('btn-share');
 const elShare = document.getElementById('article-share');
 
 btnShare.addEventListener('click', () => {
-    elShare.classList.toggle('active');
+  elShare.classList.toggle('active');
 })
